8b962a74bdccde5594d4098ca0d32a0fa469b3af
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-03 11:54:22 +0200

    Fix entitiy graph

--- dbscripts/buildClusters/collapseToEntityGraph.sql
+++ dbscripts/buildClusters/collapseToEntityGraph.sql
@@ -63,13 +63,13 @@ INSERT INTO EntityMetadata (name, indegree, outdegree, degree, distinctDegree, d
     FROM 
         (SELECT t.`to` as addr,count(*) as indegree, count(distinct t.`from`) as distinct_indegree, sum(t.amountInUSDCurrent) as involumeUSD FROM ETransfer t GROUP BY t.`to`) i;
 
-REPLACE INTO AddressMetadata (name, indegree, outdegree, degree, distinctDegree, distinctInDegree, distinctOutDegree, involumeUSD, outvolumeUSD)
+REPLACE INTO EntityMetadata (name, indegree, outdegree, degree, distinctDegree, distinctInDegree, distinctOutDegree, involumeUSD, outvolumeUSD)
     SELECT o.addr, 0, outdegree, 0+outdegree as degree, 0+distinct_outdegree as distinctDegree, 0, distinct_outdegree, 0, outvolumeUSD
     FROM 
         (SELECT t.`from` as addr,count(*) as outdegree, count(distinct t.`to`) as distinct_outdegree,  sum(t.amountInUSDCurrent) as outvolumeUSD FROM ETransfer t GROUP BY t.`from`) o;
 
 
-REPLACE INTO AddressMetadata (name, indegree, outdegree, degree, distinctDegree, distinctInDegree, distinctOutDegree, involumeUSD, outvolumeUSD)
+REPLACE INTO EntityMetadata (name, indegree, outdegree, degree, distinctDegree, distinctInDegree, distinctOutDegree, involumeUSD, outvolumeUSD)
     SELECT i.addr, indegree, outdegree, indegree+outdegree as degree, distinct_indegree+distinct_outdegree as distinctDegree, distinct_indegree, distinct_outdegree, involumeUSD, outvolumeUSD
     FROM 
         (SELECT t.`to` as addr,count(*) as indegree, count(distinct t.`from`) as distinct_indegree, sum(t.amountInUSDCurrent) as involumeUSD FROM ETransfer t GROUP BY t.`to`) i
