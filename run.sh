657e00086efa346846754831b9f28466de78f32f
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-04 19:23:54 +0200

    runscritp

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 node prepareDate/transfers/parseRawEvents
 node prepareData/transfers/splitByToken
 node prepareData/transfers/parseIndividualTokens
