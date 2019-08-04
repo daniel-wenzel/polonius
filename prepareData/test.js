dd54bf066325051fecd5f77055410afabe4bb3e6
Ubuntu <ubuntu@ip-172-31-23-219.eu-west-1.compute.internal>
2019-08-04 09:03:40 +0000

    server

--- prepareData/test.js
+++ prepareData/test.js
@@ -7,10 +7,10 @@ const gzip = zlib.createGunzip()
 gzip.on('error', (err) => {
     console.log("caught error!")
     console.error(err)
-    lineReader.end()
+    lineReader.close()
 })
 const lineReader = readline.createInterface({
-    input: fs.createReadStream('./data/ordered/0chain_0xb9ef770b6a5e12e45983c5d80545258aa38f3b78.csv.gz').pipe(gzip)
+    input: fs.createReadStream('./data/individual/0chain_0xb9ef770b6a5e12e45983c5d80545258aa38f3b78.csv.gz').pipe(gzip)
 });
 
 lineReader.on('line', (line) => {
