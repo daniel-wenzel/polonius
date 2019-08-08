1b666207cb8841f2a653434ff5fa1db51b256d66
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-08 09:46:26 +0200

    fix

--- insertIntoDB/insertTransfers/insertOperations.js
+++ insertIntoDB/insertTransfers/insertOperations.js
@@ -6,8 +6,6 @@ const insertAccounts = db.prepare('INSERT OR IGNORE INTO Address (address) VALUE
 module.exports.insertTransfers = db.transaction(transfers => {
     transfers.forEach(t => {
         try {
-            t.amountInUSD = (t.amountInUSDHigh + t.amountInUSDLow) / 2
-            t.amountInUSDCurrent = (t.amountInUSDCurrentHigh + t.amountInUSDCurrentLow) / 2
             insertTransfers.run(t)
         }
         catch (e){
