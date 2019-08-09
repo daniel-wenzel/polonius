44020c630db38c18a18d74140a4f3269eb049086
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-09 19:44:16 +0200

    fix

--- dbscripts/index.js
+++ dbscripts/index.js
@@ -4,6 +4,6 @@ require('log-timestamp');
 //require('./1_calculateDepositAddresses')
 //require('./2_labelHighDegreeAddresses')
 //require('./3_findEarlyAddresses')
-require('./4_findPaperWallets')
+//require('./4_findPaperWallets')
 //require('./5_buildClusters')
-require('./6_findICO')
+require('./6_findICOs')
