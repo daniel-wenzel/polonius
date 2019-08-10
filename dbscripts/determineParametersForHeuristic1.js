ecdbc092c1865d547c694404d31684f88762b3a7
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-10 19:57:02 +0200

    test

--- dbscripts/determineParametersForHeuristic1.js
+++ dbscripts/determineParametersForHeuristic1.js
@@ -3,14 +3,22 @@ const calculateEmptiedAddresses = requireSQL('dbscripts/calculateDepositAddresse
 const calculateDepositAddresses = requireSQL('dbscripts/calculateDepositAddresses/calculateDepositAddresses.sql')
 const success = requireSQL('dbscripts/calculateDepositAddresses/exp_determineSuccess.sql')
 
+const fs = require('fs');
+let writeStream = fs.createWriteStream('expOutput.csv');
+
 blockDelays = [180, 360, 4320, 8640, 17280, 60480]
 percentages = [0.1, 0.2, 0.25, 0.33, 0.5, 0.75]
-
+writeStream.write('numBlocks,percentage,numCappReceivers,numDepositAddresses,numFalsePositives\n')
+writeStream.flush()
 for (let numBlocks of blockDelays) {
     for (let percentage of percentages) {
         console.log(`numBlocks: ${numBlocks} percentage: ${percentage}`)
         //calculateEmptiedAddresses({numBlocks: numBlocks})
         //calculateDepositAddresses({minPercentageBehavedLikeDepositAddress: percentage})
-        console.log(success(undefined, 'get'))
+        const ans = success(undefined, 'get')
+        
+        writeStream.write([numBlocks,percentage].concat(Object.values(ans)).join(',')+"\n")
+        writeStream.flush()
     }
 }
+writeStream.end()
