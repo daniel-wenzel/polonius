44ad946a204b0687b5787c63420d90950ddefb46
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-12 08:45:50 +0200

    run3 + performance

--- dbscripts/calculateDepositAddresses/exp_determineSuccess.sql
+++ dbscripts/calculateDepositAddresses/exp_determineSuccess.sql
@@ -1,4 +1,5 @@
 SELECT 
     (SELECT count(*) FROM address where isCappReceiver = 1) as numCappReceivers,
     (SELECT count(*) FROM address where isDepositAddress = 1) as numDepositAddresses,
-    (SELECT count(*) FROM address where isDepositAddress = 1 and isExchange = 0 and name is not null) as numFalsePositives
+    (SELECT count(*) FROM address where isDepositAddress = 1 and isExchange = 0 and name is not null) as numFalsePositives,
+    (SELECT group_concat(address,';') FROM address where isDepositAddress = 1 and isExchange = 0 and name is not null) as falsePositives
