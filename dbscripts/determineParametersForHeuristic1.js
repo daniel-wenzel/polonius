f08ef1b8bd8dfbffde23dcc83d223f0dea3bdfd1
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-12 10:14:40 +0200

    fix

--- dbscripts/determineParametersForHeuristic1.js
+++ dbscripts/determineParametersForHeuristic1.js
@@ -10,12 +10,13 @@ percentages = [0.1, 0.2, 0.25, 0.33, 0.4, 0.45, 0.5, 0.6, 0.75]
 
 writeHorriblyInefficient('numBlocks,percentage,numCappReceivers,numDepositAddresses,numFalsePositives,fps')
 for (let numBlocks of blockDelays) {
-    for (let percentage of percentages)
+    for (let percentage of percentages) {
         console.log(`numBlocks: ${numBlocks} percentage: ${percentage}`)
         recalc({ minPercentageBehavedLikeDepositAddress: percentage, numBlocks })
         const ans = success(undefined, 'get')
         console.log(ans)
         writeHorriblyInefficient([numBlocks, percentage].concat(Object.values(ans)).join(','))
+    }
 }
 
 function writeHorriblyInefficient(line) {
