ce69e9dc99067ae26db457db7645c90d9464274a
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-23 11:11:08 +0200

    fix

--- dbscripts/util/commands.sql
+++ dbscripts/util/commands.sql
@@ -2,4 +2,4 @@ ALTER TABLE Entity
 ADD COLUMN "isExchange" INTEGER DEFAULT 0;
 
 UPDATE Entity
-SET isExchange = (SELECT MAX(isExchange) FROM Address WHERE cluster = Enitiy.name)
+SET isExchange = (SELECT MAX(isExchange) FROM Address WHERE cluster = Entity.name);
