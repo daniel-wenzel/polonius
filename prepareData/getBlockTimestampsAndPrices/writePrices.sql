9ae44871e128cb5bc3e4f6783f1ab1fcff3091c0
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-25 21:02:41 +0200

    potential fix

--- prepareData/getBlockTimestampsAndPrices/writePrices.sql
+++ prepareData/getBlockTimestampsAndPrices/writePrices.sql
@@ -3,7 +3,7 @@ SET amountInUSD = (
     SELECT price 
     FROM Price
     WHERE 
-        date = CAST(timestamp AS int) / 86400 * 86400
+        (""||date) = "" || CAST(timestamp AS int) / 86400 * 86400
         AND
         ETransfer.token = Price.token
     ) * amount
