547f11a0e9ca7bfdb2a671fc8f1e185aadf34a2b
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-25 21:11:00 +0200

    fix?

--- prepareData/getBlockTimestampsAndPrices/writePrices.sql
+++ prepareData/getBlockTimestampsAndPrices/writePrices.sql
@@ -1,10 +1,10 @@
 UPDATE ETransfer
-SET amountInUSD = (
+SET amountInUSD = amountInTokens * (
     SELECT price 
     FROM Price
     WHERE 
-        (""||date) = "" || CAST(timestamp AS int) / 86400 * 86400
+        date = "" || CAST(timestamp AS int) / 86400 * 86400
         AND
         ETransfer.token = Price.token
-    ) * amount
+    )
 WHERE token = @token
