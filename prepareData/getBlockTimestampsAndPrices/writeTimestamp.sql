377556a987f9e735b3bbfa45ebdf17cff4809cf2
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-26 11:46:42 +0200

    fix

--- prepareData/getBlockTimestampsAndPrices/writeTimestamp.sql
+++ prepareData/getBlockTimestampsAndPrices/writeTimestamp.sql
@@ -1,3 +1,3 @@
 UPDATE ETransfer
-SET timestamp = ROUND(@minTime + 1.0*(blocknumber - @minBlock) / (@maxBlock - @minBlock) * (@maxTime - @minTime))
+SET timestamp = (CAST ROUND(@minTime + 1.0*(blocknumber - @minBlock) / (@maxBlock - @minBlock) * (@maxTime - @minTime)) as int)
 WHERE blocknumber >= @minBlock and blocknumber < @maxBlock;
