1dcd2ad6d101a44534d126f11ffa423e7945c408
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-27 12:02:32 +0200

    fixes

--- dbscripts/7_calcTaxonomy.js
+++ dbscripts/7_calcTaxonomy.js
@@ -6,14 +6,14 @@ const getTimestamp = requireSQL('dbscripts/taxonomy/util_getTimestamp.sql')
 const getMaxBlocknumber = requireSQL('dbscripts/taxonomy/util_getMaxBlocknumber.sql')
 
 
-const steps = ['0_updateMetadata', '1_type', '2_activness', '3_operator', '4_profitability']
+const steps = ['0_updateMetadata', '1_type', '2_activeness', '3_operator', '4_profitability']
 const sqlCommands = steps.map(s => requireSQL('dbscripts/taxonomy/'+s+".sql"))
 const createTaxonomy = (blocknumber) => {
     if (blocknumber == null) {
         blocknumber = getMaxBlocknumber({}, 'get')
     }
     const timestamp = getTimestamp({blocknumber},'get')
-    sqlCommands.map(sql => sql({blocknumber, timestamp}))
+    //sqlCommands.map(sql => sql({blocknumber, timestamp}))
 }
-init()
+//init()
 createTaxonomy()
