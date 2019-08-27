3751ca23cbbc9376563e0296ae60280bda3bf082
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-27 16:55:16 +0200

    parents & children

--- dbscripts/7_calcTaxonomy.js
+++ dbscripts/7_calcTaxonomy.js
@@ -6,7 +6,7 @@ const getTimestamp = requireSQL('dbscripts/taxonomy/util_getTimestamp.sql')
 const getMaxBlocknumber = requireSQL('dbscripts/taxonomy/util_getMaxBlocknumber.sql')
 
 
-const steps = [/*'0_updateMetadata',*/ '1_type', '2_activeness', '3_operator', '4_profitability']
+const steps = [/*'0_updateMetadata', '1_type', '2_activeness', '3_operator', '4_profitability'*/, '5_parentschildren']
 const sqlCommands = steps.map(s => requireSQL('dbscripts/taxonomy/'+s+".sql"))
 const createTaxonomy = (blocknumber) => {
     if (blocknumber == null) {
---
+++ dbscripts/taxonomy/5_parentschildren.sql
@@ -0,0 +1,59 @@
+CREATE TEMP TABLE parents AS
+SELECT 
+    e.name, 
+    parentT.type
+FROM 
+    EntityMetadata m
+    INNER JOIN
+    ETransfer t
+    INNER JOIN
+    (Entity parent
+    NATURAL JOIN
+    EntityTaxonomy parentT)
+ON 
+    m.name = t.`to` and 
+    t.`from` = parent.name and
+    parentT.blocknumber = @blocknumber and
+    t.blocknumber <= @ blocknumber
+GROUP BY e.name, parentT.type
+HAVING sum(amountInUSDCurrent) > 0.75 * involumeUSD;
+
+CREATE TEMP TABLE children AS
+SELECT 
+    e.name, 
+    childT.type
+FROM 
+    EntityMetadata m
+    INNER JOIN
+    ETransfer t
+    INNER JOIN
+    (Entity child
+    NATURAL JOIN
+    EntityTaxonomy childT)
+ON 
+    m.name = t.`from` and 
+    t.`to` = child.name and
+    childT.blocknumber = @blocknumber and
+    t.blocknumber <= @ blocknumber
+GROUP BY e.name, childT.type
+HAVING sum(amountInUSDCurrent) > 0.75 * outvolumeUSD;
+
+CREATE INDEX Children_name ON children("name");
+CREATE INDEX Parents_name ON parents("name");
+
+UPDATE EntityTaxonomy
+SET children = (SELECT type FROM children WHERE children.name = EntityTaxonomy.name)
+SET parents = (SELECT type FROM parents WHERE parents.name = EntityTaxonomy.name)
+WHERE blocknumber = @blocknumber;
+
+UPDATE EntityTaxonomy
+SET children = 'none'
+WHERE blocknumber = @blocknumber and name in (SELECT name from EntityMetadata where distinctOutDegree = 0);
+
+UPDATE EntityTaxonomy
+SET parents = 'mixed'
+WHERE blocknumber = @blocknumber and parents is null;
+
+UPDATE EntityTaxonomy
+SET children = 'mixed'
+WHERE blocknumber = @blocknumber and children is null;
