07c99f919704083439dfef56e1a22e7b5a832f2d
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-27 15:44:45 +0200

    tests

--- dbscripts/taxonomy/4_profitability.sql
+++ dbscripts/taxonomy/4_profitability.sql
@@ -79,5 +79,8 @@ GROUP BY name;
 
 CREATE INDEX Profits_name ON Profits("name");
 
+UPDATE EntityMetadata
+SET profitability = null;
+
 UPDATE EntityMetadata 
 SET profitability = (SELECT Profits.profitPercentage FROM Profits WHERE Profits.name = EntityMetadata.name);
