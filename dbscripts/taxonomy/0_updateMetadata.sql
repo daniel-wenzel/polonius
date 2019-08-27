e60fbcd36ec015ceec72b2cabc7fdacdba9d6a98
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-27 12:13:47 +0200

    bugfixes

--- dbscripts/taxonomy/0_updateMetadata.sql
+++ dbscripts/taxonomy/0_updateMetadata.sql
@@ -27,38 +27,38 @@ GROUP BY name;
 CREATE INDEX inTemp_name ON inTemp("name");
 CREATE INDEX outTemp_name ON outTemp("name");
 
-UPDATE EntityMetadata
-SET indegree = (SELECT indegree FROM inTemp WHERE inTemp.name = EntityMetadata.name)
-SET distinct_indegree = (SELECT indegree FROM inTemp WHERE inTemp.name = EntityMetadata.name)
-SET involumeUSD = (SELECT involumeUSD FROM inTemp WHERE inTemp.name = EntityMetadata.name)
-SET firstInTransfer = (SELECT firstInTransfer FROM inTemp WHERE inTemp.name = EntityMetadata.name)
-SET lastInTransfer = (SELECT lastInTransfer FROM inTemp WHERE inTemp.name = EntityMetadata.name)
+UPDATE EntityMetadata SET 
+    indegree = (SELECT indegree FROM inTemp WHERE inTemp.name = EntityMetadata.name),
+    distinct_indegree = (SELECT indegree FROM inTemp WHERE inTemp.name = EntityMetadata.name),
+    involumeUSD = (SELECT involumeUSD FROM inTemp WHERE inTemp.name = EntityMetadata.name),
+    firstInTransfer = (SELECT firstInTransfer FROM inTemp WHERE inTemp.name = EntityMetadata.name),
+    lastInTransfer = (SELECT lastInTransfer FROM inTemp WHERE inTemp.name = EntityMetadata.name)
 WHERE EntityMetadata.name in (SELECT name from inTemp);
 
-UPDATE EntityMetadata
-SET indegree = 0
-SET distinct_indegree = 0
-SET involumeUSD = 0
-SET firstInTransfer = null
-SET lastInTransfer = null
+UPDATE EntityMetadata SET
+    indegree = 0,
+    distinct_indegree = 0,
+    involumeUSD = 0,
+    firstInTransfer = null,
+    lastInTransfer = null
 WHERE EntityMetadata.name not in (SELECT name from inTemp);
 
-UPDATE EntityMetadata
-SET outdegree = (SELECT indegree FROM outTemp WHERE outTemp.name = EntityMetadata.name)
-SET distinct_outdegree = (SELECT indegree FROM outTemp WHERE outTemp.name = EntityMetadata.name)
-SET outvolumeUSD = (SELECT involumeUSD FROM outTemp WHERE outTemp.name = EntityMetadata.name)
-SET firstOutTransfer = (SELECT firstOutTransfer FROM outTemp WHERE outTemp.name = EntityMetadata.name)
-SET lastOutTransfer = (SELECT lastOutTransfer FROM outTemp WHERE outTemp.name = EntityMetadata.name)
+UPDATE EntityMetadata SET
+    outdegree = (SELECT indegree FROM outTemp WHERE outTemp.name = EntityMetadata.name),
+    distinct_outdegree = (SELECT indegree FROM outTemp WHERE outTemp.name = EntityMetadata.name),
+    outvolumeUSD = (SELECT involumeUSD FROM outTemp WHERE outTemp.name = EntityMetadata.name),
+    firstOutTransfer = (SELECT firstOutTransfer FROM outTemp WHERE outTemp.name = EntityMetadata.name),
+    lastOutTransfer = (SELECT lastOutTransfer FROM outTemp WHERE outTemp.name = EntityMetadata.name)
 WHERE EntityMetadata.name in (SELECT name from outTemp);
 
-UPDATE EntityMetadata
-SET outdegree = 0
-SET distinct_outdegree = 0
-SET outvolumeUSD = 0
-SET firstInTransfer = null
-SET lastInTransfer = null
+UPDATE EntityMetadata SET
+    outdegree = 0,
+    distinct_outdegree = 0,
+    outvolumeUSD = 0,
+    firstInTransfer = null,
+    lastInTransfer = null
 WHERE EntityMetadata.name not in (SELECT name from outTemp);
 
-UPDATE EntityMetadata
-SET degree = indegree + outdegree;
-SET distinctDegree = distinct_indegree + distinct_outdegree; 
+UPDATE EntityMetadata SET
+    degree = indegree + outdegree,
+    distinctDegree = distinct_indegree + distinct_outdegree; 
--- dbscripts/taxonomy/2_activeness.sql
+++ dbscripts/taxonomy/2_activeness.sql
@@ -2,9 +2,9 @@ UPDATE EntityTaxonomy
 SET activeness = (
     SELECT CASE
         WHEN involumeUSD - outvolumeUSD BETWEEN -0.05 and 0.05 THEN "empty"
-        WHEN @maxDate - MAX(lastInTransfer, lastOutTransfer) < 1000 * 60 * 60 * 24 THEN "daily"
-        WHEN @maxDate - MAX(lastInTransfer, lastOutTransfer) < 1000 * 60 * 60 * 24 * 7 THEN "weekly"
-        WHEN @maxDate - MAX(lastInTransfer, lastOutTransfer) < 1000 * 60 * 60 * 24 * 30 THEN "monthly"
+        WHEN @maxDate - MAX(IFNULL(lastInTransfer,0), IFNULL(lastOutTransfer,0)) < 1000 * 60 * 60 * 24 THEN "daily"
+        WHEN @maxDate - MAX(IFNULL(lastInTransfer,0), IFNULL(lastOutTransfer,0)) < 1000 * 60 * 60 * 24 * 7 THEN "weekly"
+        WHEN @maxDate - MAX(IFNULL(lastInTransfer,0), IFNULL(lastOutTransfer,0)) < 1000 * 60 * 60 * 24 * 30 THEN "monthly"
         ELSE "inactive"
     FROM
         EntityMetadata
