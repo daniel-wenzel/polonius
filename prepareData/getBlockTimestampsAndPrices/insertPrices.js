6c95d3f06634a9d94f99bc9528c1e3b9d0509d44
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-28 20:03:20 +0200

    fix

--- prepareData/getBlockTimestampsAndPrices/insertPrices.js
+++ prepareData/getBlockTimestampsAndPrices/insertPrices.js
@@ -18,13 +18,13 @@ async function insertPricesForToken(token) {
 }
 async function run() {
     try {
-        /*createPricingTable()
+        createPricingTable()
         const tokens = getTokens(undefined, 'all')
-        console.log(tokens.length+" tokens found")
+        /*console.log(tokens.length+" tokens found")
         for (const {token} of tokens) {
             await insertPricesForToken(token)
-        }*/
-        insertMissingPrices()
+        }
+        insertMissingPrices()*/
         for (const {token} of tokens) {
             writePrices({token})
         }
