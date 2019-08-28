ee4e800f57a4adee4438aff9b276d5778fff154e
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-28 20:01:16 +0200

    complete recalc

--- dbscripts/7_calcTaxonomy.js
+++ dbscripts/7_calcTaxonomy.js
@@ -6,15 +6,15 @@ const getTimestamp = requireSQL('dbscripts/taxonomy/util_getTimestamp.sql')
 const getMaxBlocknumber = requireSQL('dbscripts/taxonomy/util_getMaxBlocknumber.sql')
 
 
-const steps = [/*
+const steps = [
     '0_updateMetadata', 
     '1_type', 
     '2_activeness', 
-    '3_operator', */
-    '4_profitability', /*
+    '3_operator', 
+    '4_profitability', 
     '5_parentschildren',
     '6_holdingSize',
-    '7_numberOfTokens'*/
+    '7_numberOfTokens'
 ]
 const sqlCommands = steps.map(s => requireSQL('dbscripts/taxonomy/'+s+".sql"))
 const createTaxonomy = (blocknumber) => {
