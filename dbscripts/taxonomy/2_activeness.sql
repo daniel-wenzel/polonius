5d41ae40558359fc9e0aac0bb372e805a6b0adea
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-08-29 18:55:18 +0200

    activeness

--- dbscripts/taxonomy/2_activeness.sql
+++ dbscripts/taxonomy/2_activeness.sql
@@ -1,11 +1,12 @@
 UPDATE EntityTaxonomy
 SET activeness = (
     SELECT CASE
-        WHEN involumeUSD - outvolumeUSD BETWEEN -0.05 and 0.05 THEN "empty"
         WHEN @timestamp - MAX(IFNULL(lastInTransfer,0), IFNULL(lastOutTransfer,0)) < 60 * 60 * 24 THEN "daily"
         WHEN @timestamp - MAX(IFNULL(lastInTransfer,0), IFNULL(lastOutTransfer,0)) < 60 * 60 * 24 * 7 THEN "weekly"
         WHEN @timestamp - MAX(IFNULL(lastInTransfer,0), IFNULL(lastOutTransfer,0)) < 60 * 60 * 24 * 30 THEN "monthly"
-        ELSE "inactive"
+        WHEN @timestamp - MAX(IFNULL(lastInTransfer,0), IFNULL(lastOutTransfer,0)) >= 60 * 60 * 24 * 30 and involumeUSD - outvolumeUSD BETWEEN -0.05 and 0.05 THEN "inactive_empty"
+        WHEN @timestamp - MAX(IFNULL(lastInTransfer,0), IFNULL(lastOutTransfer,0)) >= 60 * 60 * 24 * 30 THEN "inactive_not empty"
+        ELSE null
     END 
     FROM
         EntityMetadata
