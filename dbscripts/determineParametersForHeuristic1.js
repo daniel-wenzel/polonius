f741d7c80c382971fc02cfa850e4cd2faf9677ca
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2019-09-07 14:08:46 +0200

    set clustering values to chosen ones

--- dbscripts/determineParametersForHeuristic1.js
+++ dbscripts/determineParametersForHeuristic1.js
@@ -6,8 +6,8 @@ const success = requireSQL('dbscripts/calculateDepositAddresses/exp_determineSuc
 
 const fs = require('fs');
 
-blockDelays = [180, 360, 4320, 8640, 17280, 25920, 34560, 43200, 60480, 51840]
-percentages = [0.1, 0.2, 0.25, 0.33, 0.4, 0.45, 0.6, 0.75, 0.5]
+blockDelays = [60480] //[180, 360, 4320, 8640, 17280, 25920, 34560, 43200, 51840, 60480]
+percentages = [0.5] //[0.1, 0.2, 0.25, 0.33, 0.4, 0.45, 0.6, 0.75, 0.5]
 
 writeHorriblyInefficient('numBlocks,percentage,numCappReceivers,numDepositAddresses,numFalsePositives,fps')
 for (let numBlocks of blockDelays) {
@@ -22,7 +22,7 @@ for (let numBlocks of blockDelays) {
 }
 
 function writeHorriblyInefficient(line) {
-    let writeStream = fs.createWriteStream('expOutput_v4.csv', {'flags': 'a'});
+    let writeStream = fs.createWriteStream('expOutput_v4_2.csv', {'flags': 'a'});
     writeStream.write(line+"\n")
     writeStream.end()
 }
